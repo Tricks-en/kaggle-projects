541bc18364a7a4311a68655d827b27da2284463b
Tricks <114878013+Tricks-en@users.noreply.github.com>
2025-01-30 10:55:36 -0500

    Boston houses 1.1

--- Boston_Houses.py
+++ Boston_Houses.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 from sklearn import linear_model
 
 # Load kaggle dataset from cache into pandas
-BH_data = pd.read_csv("C:/Users/trick/.cache/kagglehub/datasets/vikrishnan/boston-house-prices/versions/1/housing.csv", delimiter=" ")
+BH_data = pd.read_csv("C:/Users/trick/.cache/kagglehub/datasets/vikrishnan/boston-house-prices/versions/1/housing.csv", delimiter=",")
+BH_data = BH_data.astype(float)
 # check data for missing data, errors, etc.
 print(BH_data.info())
 print(BH_data.corr())
@@ -15,6 +16,7 @@ y = BH_data[['MEDV']]
 Multi_reg = linear_model.LinearRegression()
 Multi_reg.fit(X, y)
 
+# linear regression over scatter plot for visualization
 def lr_scatter(x, y, xlabel, ylabel):
   reg = linear_model.LinearRegression()
   reg.fit(x, y)
@@ -25,15 +27,16 @@ def lr_scatter(x, y, xlabel, ylabel):
   plt.xlabel(xlabel)
   plt.show()
 
-""" lr_scatter(BH_data[['INDUS']], y, "proportion of non-retail business acres per town", "Home Value $/thousands")
+# visualize single var regressions over home value
+lr_scatter(BH_data[['INDUS']], y, "proportion of non-retail business acres per town", "Home Value $/thousands")
 lr_scatter(BH_data[['NOX']], y, "nitric oxides concentration (pp10m)", "Home Value $/thousands")
 lr_scatter(BH_data[['RM']], y, "# of rooms/dwelling", "Home Value $/thousands")
 lr_scatter(BH_data[['TAX']], y, "property-tax rate per $10k", "Home Value $/thousands")
 lr_scatter(BH_data[['PTRATIO']], y, "pupil-teacher ratio by town", "Home Value $/thousands")
-lr_scatter(BH_data[['LSTAT']], y, "% lower status of the population", "Home Value $/thousands") """
+lr_scatter(BH_data[['LSTAT']], y, "% lower status of the population", "Home Value $/thousands")
 
 X_test = [[12.5,	0.524,	5.889, 311,	15.2,	15.71]]
 multi_y_pred = Multi_reg.predict(X_test)
-actual_MEDV = "21.7"
+# actual_MEDV = "21.7"
 
-print(multi_y_pred, actual_MEDV)
+print(multi_y_pred)
